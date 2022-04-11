9ee21aeb48d0e29aaed8b80dcb234d6c70d6bdf0
Nitin Murthy <>
2022-04-11 18:37:29 +0000

    Made nodeJS run properly with a readable environment and formatting

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,4 +1,5 @@
 //Code credit: https://www.geeksforgeeks.org/how-to-insert-text-into-the-textarea-at-the-current-cursor-position/#:~:text=First%2C%20get%20the%20current%20position,and%20end%20of%20the%20text.
+/*
 document.getElementById('code').addEventListener('keydown', function (e) {
   if(e.keyCode==9) {
     console.log('GOT HERE');
@@ -9,6 +10,7 @@ document.getElementById('code').addEventListener('keydown', function (e) {
 x.slice(0, curPos) + '\t' + x.slice(curPos));
   }
 })
+*/
 
 window.onload = function () {
   var editor = CodeMirror.fromTextArea($("#code")[0], {
@@ -16,4 +18,13 @@ window.onload = function () {
       lineWrapping: true,
       mode: "javascript",
   });
+  editor.setSize('100%','90%');
+  var output = document.getElementById('output');
+  output.style.height = editor.getWrapperElement().offsetHeight;
 };
+
+function setOutputStyle(){
+  output.setAttribute('overflow-y','auto');
+  output.style.margin = "10px";
+  output.style.border = "border: 1px solid black;";
+}
