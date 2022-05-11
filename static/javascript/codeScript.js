f358885172ba2e6dc36a315fb2e6c94a265a74b4
NitinMurthy <>
2022-05-11 12:31:28 -0500

    Adding support to edit markdowns

--- static/javascript/codeScript.js
+++ static/javascript/codeScript.js
@@ -4,22 +4,32 @@ window.onload = function () {
       lineWrapping: true,
       mode: "javascript",
   });
+  var editor2 = CodeMirror.fromTextArea($("#mkdown")[0], {
+    lineNumbers: true,
+    lineWrapping: true,
+    mode: "markdown"
+  });
+  editor.setSize('100%','95%');
   editor.setSize('100%','95%');
   var output = document.getElementById('output');
   output.style.height = editor.getWrapperElement().offsetHeight;
   code = editor.getValue();
   
   editor.on('change',save);
+  editor2.on('change',save);
 };
 
 function save(){
   var editor = document.getElementById('code').nextSibling.CodeMirror;
   code = editor.getValue();
   document.getElementById('save').textContent = 'Saving';
+  var editor2 = document.getElementById('mkdown').nextSibling.CodeMirror;
+  var markdownValue = editor2.getValue();
   var server_data = [
     {"Name": document.getElementById('docName').value},
     {"docID": window.location.href.split('/code/')[1]},
-    {"code": code}
+    {"code": code},
+    {"markdown": markdownValue}
   ];
   $.ajax({
     type: "POST",
@@ -32,6 +42,7 @@ function save(){
       if(document.getElementById('docName').value != '')
         document.getElementById('title').textContent = document.getElementById('docName').value;
       else document.getElementById('title').textContent = 'Untitled project';
+      document.getElementById('markdown').innerHTML = markdownValue;
     }
   });
 }
@@ -48,17 +59,25 @@ function runCode(){
 }
 
 function switchTab(string){
-  var md = document.getElementById('markdown');
-  var console = document.getElementById('console');
-  var mdTab = document.getElementById('markdownTab');
-  var consoleTab = document.getElementById('consoleTab')
-  if(string == 'markdown'){
+  if(string == 'markdown' || string == 'console'){
+    var md = document.getElementById('markdown');
+    var console = document.getElementById('console');
+    var mdTab = document.getElementById('markdownTab');
+    var consoleTab = document.getElementById('consoleTab');
+  }
+  else{
+    var md = document.getElementById('mkdown');
+    var console = document.getElementById('code');
+    var mdTab = document.getElementById('markdownEditTab');
+    var consoleTab = document.getElementById('inputTab');
+  }
+  if(string == 'markdown' || string == 'mkdown'){
     console.style.display = 'none';
     markdown.style.display = 'inline-block';
     mdTab.setAttribute('class','nav-link active');
     consoleTab.setAttribute('class','nav-link');
   }
-  else{
+  else if(string=='console' || string == 'input'){
     console.style.display = 'inline-block';
     markdown.style.display = 'none';
     mdTab.setAttribute('class','nav-link');
